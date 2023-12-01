28898152e08be33d7df891b96afcf57808fda437
rkdgus0810 <hyun0810d@gmail.com>
2023-12-01 02:21:55 -0800

    threading complete / ToDO:socket combine

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Servo_Controller_Class:
         self.m_ZeroOffset = ZeroOffset
 
         self.mPwm = Adafruit_PCA9685.PCA9685(address = 0x40)
-        self.mPwm.set_pwm_freq(50)
+        self.mPwm.set_pwm_freq(60)
     
     def SetPos(self,pos):
         pulse = (650-150)*pos/180+150+self.m_ZeroOffset
@@ -29,15 +29,35 @@ class Servo_Controller_Class:
 Servo_controller = []
 Servo_action_thread = []
 action_range = [[0]]
+action_lock=[threading.Lock() for _ in action_range]
 
-def servo_action(controller,pos,delay)
+def servo_action(controller,pos,index):
+    global action_range
+    while True:
+        controller.SetPos(pos[0])
+        action_lock[index].acquire
+        if len(action_range[index]) > 1:
+            action_range[index].pop(0)
 
 if __name__ == '__main__':
-    for i in range(11):
-        Servo_controller.append(Servo_Controller_Class(Channel = i, ZeroOffset = -10)) 
+    channel=0
+    for index in range(11):
+        channel=index
+        if index>4:
+            channel+=1
+        Servo_controller.append(Servo_Controller_Class(Channel = channel, ZeroOffset = -10)) 
+        print(f"Init Controller No.{channel}")
     
+    index=0
     for controller_thread in Servo_controller:
-        Servo_action_thread.append(threading.Thread(target = servo_action, args = (controller_thread,)))
+        Servo_action_thread.append(threading.Thread(target = servo_action, args = (controller_thread,action_range[index],index)))
+        index+=1
+
+    index=0
+    for controller_thread in Servo_action_thread:
+        controller_thread.start()
+        print(f"Start of Motor No.{index}")
+        index+=1
 
     try:
         pass
@@ -46,4 +66,5 @@ if __name__ == '__main__':
     except Exception as e:
         print(Colors.RED+"Exception error: "+Colors.END+str(e))
     finally:
-        Servo.Cleanup()
+        for controller in Servo_controller:
+            controller.Cleanup()
---
+++ test.py
@@ -0,0 +1,47 @@
+import time
+import RPi.GPIO as GPIO
+import Adafruit_PCA9685
+
+class SG90_92R_Class:
+
+    def __init__(self, Channel, ZeroOffset):
+        self.mChannel = Channel
+        self.m_ZeroOffset = ZeroOffset
+
+        self.mPwm = Adafruit_PCA9685.PCA9685(address = 0x40)
+        self.mPwm.set_pwm_freq(60)
+
+    def SetPos(self, pos):
+        pulse = (650 - 150) * pos / 180 + 150 + self.m_ZeroOffset
+        self.mPwm.set_pwm(self.mChannel, 0, int(pulse))
+
+    def Cleanup(self):
+        self.SetPos(90)
+        time.sleep(1)
+
+if __name__ == '__main__':
+    Servo = SG90_92R_Class(Channel = 0, ZeroOffset = -10)
+
+    angle_max = 90
+    angle_delay_t = 0.01
+    a=0
+    
+    try:
+        while True:
+    #         for i in range(angle_max):
+    #             time.sleep(angle_delay_t)
+    #             Servo.SetPos(i)
+    #         for i in reversed(range(angle_max)):
+    #             time.sleep(angle_delay_t)
+    #             Servo.SetPos(i)
+            a = input()
+            Servo.SetPos(int(a))
+
+    except KeyboardInterrupt:
+        print("Ctrl + C")
+
+    except Exception as e:
+        print(str(e))
+
+    # finally:
+    #     Servo.Cleanup()
